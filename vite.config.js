5462c81651eb60c86a8cd100003b11dbab1c9011
Dominik Morse <aenszguhelem@gmail.com>
2026-05-12 13:19:36 +0200

    Updated deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { dirname, resolve } from 'path';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
